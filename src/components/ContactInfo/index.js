7d4e2a0177fd00b3a0601ae6ded24cb871ac04bc
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2020-05-13 15:54:43 -0300

    Created ContactInfo Component

---
+++ src/components/ContactInfo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.scss";
+
+function ContactInfo() {
+  return (
+    <div className="contact-info">
+      <h3 className="contact-info-title">Seus contatos</h3>
+      <small className="contact-info-count">98 contatos</small>
+    </div>
+  );
+}
+
+export default ContactInfo;
