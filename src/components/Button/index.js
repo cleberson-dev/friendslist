440e1d26d967ecc50763490b3cb56fe82eddeef3
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2020-05-13 15:55:09 -0300

    Created Button Component

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+function Button() {
+  return (
+    <button className="button">
+      + Novo
+    </button>
+  );
+}
+
+export default Button;
