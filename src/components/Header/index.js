c1924460823f2557c9c66625130c026556a4cd52
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2020-05-13 15:21:09 -0300

    Added Header Component

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <h3 className="header-title">friendslist</h3>
+    </header>
+  );
+}
+
+export default Header
