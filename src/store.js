34a641c6331f83b571d8afe324a601e854cdb5d1
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2020-05-15 21:56:35 -0300

    Implemented Global Store with Hooks and Context

---
+++ src/store.js
@@ -0,0 +1,59 @@
+import { createContext } from "react";
+
+export const initialState = {
+  modal: {
+    isOpen: false,
+    type: ''
+  },
+  editContact: { name: '', telephone: '', photo: '' },
+  contacts: [
+    // { id: 1, name: "Cleberson Junior", telephone: "+55 98 98507-6563", photo: "" },
+    // { id: 2, name: "Cleberson Junior", telephone: "+55 98 98507-6563", photo: "" },
+    // { id: 3, name: "Cleberson Junior", telephone: "+55 98 98507-6563", photo: "" }
+  ]
+};
+
+export function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE_CONTACT':
+      const { name, telephone, photo } = action.payload;
+        const newContact = { 
+          id: state.contacts.length + 1, 
+          name, telephone, photo 
+        };
+        return { ...state, contacts: [...state.contacts, newContact] };
+
+    case 'EDIT_CONTACT_REQUEST':
+      return { ...state, editContact: {
+        id: action.payload.id,
+        name: action.payload.name,
+        telephone: action.payload.telephone,
+        photo: action.payload.photo
+      }};
+
+    case 'EDIT_CONTACT':
+      const idx = state.contacts.findIndex(contact => contact.id === action.payload.id);
+      state.contacts[idx].name = action.payload.name;
+      state.contacts[idx].telephone = action.payload.telephone;
+      state.contacts[idx].photo = action.payload.photo;
+
+      return { ...state, contacts: state.contacts };
+    
+    case 'REMOVE_CONTACT':
+      return {
+        ...state, 
+        contacts: state.contacts.filter(contact => contact.id !== action.payload)
+      };
+    
+    case 'OPEN_MODAL':
+      return { ...state, modal: { isOpen: true, type: action.payload } };
+
+    case 'CLOSE_MODAL':
+      return { ...state, modal: { isOpen: false, type: '' } };
+
+    default:
+      return state;
+  }
+}
+
+export const AppContext = createContext();
