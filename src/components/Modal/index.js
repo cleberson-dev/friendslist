e78a9b41886cc1bb884b013f8b5ed4d12a0baf33
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2020-05-15 21:54:44 -0300

    Added actions section to Modal Component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./styles.scss";
+import { ReactComponent as CloseIcon } from "../../img/close.svg";
 
-function Modal({ children }) {
+import IconButton from "../IconButton";
+
+function Modal({ children, onExitHandler = (e) => e.preventDefault() }) {
   return (
     <div className="modal">
+      <div className="actions">
+        <IconButton onClick={onExitHandler} icon={<CloseIcon />} />
+      </div>
       { children }
     </div>
   );
