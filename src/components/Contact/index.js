c84053951408c6df7035eb364d7eeb7ba8ea47ba
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2020-05-15 21:58:11 -0300

    Edited Contact Component to support app state

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./styles.scss";
+import { AppContext } from "../../store";
 
 import IconButton from "../IconButton";
 
@@ -9,11 +10,14 @@ import { ReactComponent as EditIcon } from '../../img/edit.svg';
 const isPhoto = (photoSrc) => photoSrc && photoSrc !== "";
 const defaultPhoto = "https://interfacetreinamentos.com.br/wp-content/uploads/2016/04/img-profile-default.jpg";
 
-function Contact({ 
+function Contact({
+  id, 
   photo, 
   name, 
   telephone 
 }) {
+  const { dispatch } = useContext(AppContext);
+
   return (
     <div className="contact">
       <img 
@@ -26,8 +30,19 @@ function Contact({
         <a href={`tel:${telephone}`} className="contact-telephone">{telephone}</a>
       </address>
       <div className="contact-actions">
-        <IconButton icon={<EditIcon />} />
-        <IconButton icon={<DeleteIcon />} />
+        <IconButton 
+          icon={<EditIcon />}
+          onClick={e => {
+            dispatch({ type: 'EDIT_CONTACT_REQUEST', payload: {
+              id, name, telephone, photo
+            }});
+            dispatch({ type: 'OPEN_MODAL', payload: 'edit' });
+          }} 
+        />
+        <IconButton 
+          icon={<DeleteIcon />} 
+          onClick={e => dispatch({ type: 'REMOVE_CONTACT', payload: id })} 
+        />
       </div>
     </div>
   );
